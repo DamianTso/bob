606d48c18c6b1ba4835e49c3e0a686f1e896adda
DamianTso <42222242+DamianTso@users.noreply.github.com>
2018-08-09 14:47:25 -0700

    Add files via upload

---
+++ 180808.py
@@ -0,0 +1,10 @@
+print("hey there buddy you look lost")
+input("yes or no")
+print("\_(1_1)_/")
+print("    |    ")
+print("    |    ")
+print("    |    ")
+print("   / \   ")
+print("  /   \  ")
+
+
